79ad3559e9609ce35ff4602e45ad0a8251d5caa7
AkanshiGupta09 <guptaakanshi72@gmail.com>
2024-11-07 13:58:29 +0530

    divide description and dataset added

--- src/i18n/en/localised-strings.ts
+++ src/i18n/en/localised-strings.ts
@@ -3,11 +3,11 @@ export const localised = {
   language_hindi: 'हिन्दी',
   language_english: 'English',
   language_changed: 'Language changed to English',
-  welcomeMessage: "😊Welcome to the Musical Adventure! Ready to discover amazing instruments?🎵",
+  welcomeMessage: "😊Welcome to the Musical Adventure! ✨Ready to discover amazing instruments?🎵",
   validText: ['hi', 'Hi', 'HI', 'hI', 'Hello', 'hello', 'hola'],
-  selectSubtopic: (topicName: string) =>`"Awesome choice! Here’s what you can learn about **${topicName}**:"`,
+  selectSubtopic: (topicName: string) =>`😊Awesome choice! 📝Here’s what you can learn about 💬**${topicName}**:`,
   mainMenu:'Main Menu',
-  chooseTopic:"Great! Let’s dive into the world of musical instruments. Which family would you like to explore? 🎶✨",
+  chooseTopic:"Great!🎉 Let’s dive into the world of 🎶musical instruments. Which family would you 💯like to explore? ✨",
   retakeQuiz:'Retake Quiz',
   startQuiz: 'Start Quiz',
   Moreexplanation:'More Explanation',
@@ -15,15 +15,11 @@ export const localised = {
   endMessage:"Whenever you're ready to continue, just type 'Hi' to start the bot again. Looking forward to helping you out! 😊",
   explanation: (subtopicName: string, description: string) =>
   `📖 **Explanation of ${subtopicName}:**\n${description}`,
-  moreExplanation: (subtopicName: string, description: string) =>
-  `📝 More Explanation of **${subtopicName}:**\n${description}`,
+  moreExplanation: (subtopicName: string, description: string) =>`📝 More Explanation of **${subtopicName}:**\n${description}`,
   difficulty: `🎯 Choose your quiz level to get started!🚀`,
-  rightAnswer: (explanation: string) =>
-  `🌟 Fantastic! You got it 👍right!\nCheck this out: **${explanation}**`,
-  wrongAnswer: (correctAnswer: string, explanation: string) =>
- `👎Not quite right, but you’re learning! 💪\nThe correct answer is: **${correctAnswer}**\nHere’s the explanation: **${explanation}**`,
-  score: (score: number, totalQuestions: number,  badge:string) =>
-  `🌟 Wow! You did an awesome job. **${score}** out of **${totalQuestions}**.\n\n💪 Congratulations! You earned ${ badge} badge! `,
+  rightAnswer: (explanation: string) =>`🌟 Fantastic! You got it 👍right!\nCheck this out:🎯 **${explanation}**`,
+  wrongAnswer: (correctAnswer: string, explanation: string) =>`👎Not quite right, but you’re learning! 💪\nThe correct answer is: 💯**${correctAnswer}**\nHere’s the explanation: **${explanation}**`,
+  score: (score: number, totalQuestions: number,  badge:string) =>`🌟 Wow! You did an awesome job. **${score}** out of **${totalQuestions}**.\n\n💪 Congratulations! You earned ${ badge} badge! `,
    
   
 };
